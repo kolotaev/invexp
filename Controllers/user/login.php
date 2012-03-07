df6bb40f26944a2bd7da07360eedba8217887025
Kolotaev <kolotaev@tut.by>
2012-03-07 10:40:57 +0300

    Beans_XBean ideas

--- Controllers/user/login.php
+++ Controllers/user/login.php
@@ -8,6 +8,12 @@ class ControllerLogin extends ControllerBase {
 	public function index() {
 	//session_start();
 	//$this->registry->get('template')->set('first_name', $_SESSION['xml']);
+	
+	//$a = BeanFactory::build("XBean");
+	$a = BeanFactory::build("Beans_X");
+	$a->test();
+	$b = new Beans_XBean();
+	$b->test();
 	$this->registry->get('template')->show('users/login-form');
 	}
 	 
