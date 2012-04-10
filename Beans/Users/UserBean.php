004157664ba51be496c2450a742a86659a203d29
kolotaev <kolotaev@tut.by>
2012-04-10 22:50:55 +0200

    mongo-serv + index.php reverted

--- Beans/Users/UserBean.php
+++ Beans/Users/UserBean.php
@@ -10,6 +10,7 @@ class UserBean {
         $date = date("Y-m-d");
 
         $statement = "INSERT INTO Users(login, email, pass, reg_date) VALUES ('$login','$mail','$pass', '$date')";
+        $statement = mysql_real_escape_string($statement);
         mysql_query($statement);
 
     }
